4121ab9f9b4882d024403a10a166e657aee08023
samfeeng <samfe5040@gmail.com>
2024-09-11 22:29:44 +0100

    issues 60 and 59 are resolved

--- src/components/connect-wallet-modal/index.tsx
+++ src/components/connect-wallet-modal/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import {
   Modal,
   ModalOverlay,
@@ -8,16 +8,20 @@ import {
   Image,
 } from "@chakra-ui/react";
 import { UnifiedWalletButton } from "@jup-ag/wallet-adapter";
+import { useAnchorWallet, useWallet } from "@solana/wallet-adapter-react";
+import { useUmi } from "../../web3/solana/hook";
 
 interface ConnectWalletModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
+
 const ConnectWalletModal: FC<ConnectWalletModalProps> = ({
   isOpen,
   onClose,
 }) => {
+
   return (
     <Modal
       onClose={onClose}
